82c0a37bb2022c0750ea0a783f3b88379e71adca
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2022-02-22 21:51:33 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,19 +1,27 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable{
+	
+	private DepartmentService service;
+	private ObservableList<Department> obsList;
 	
 	@FXML private TableView<Department> tableViewDepartment;
 	@FXML private TableColumn<Department, Integer> tableColumnId;
@@ -30,6 +38,12 @@ public class DepartmentListController implements Initializable{
 		initializeNodes();
 	}
 
+	
+	
+	public void setService(DepartmentService service) {
+		this.service = service;
+	}
+
 
 	private void initializeNodes() {
 		
@@ -44,6 +58,17 @@ public class DepartmentListController implements Initializable{
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 		tableViewDepartment.prefWidthProperty().bind(stage.widthProperty());
 		
+	}
+	
+	public void updateTableView() {
+		if(service == null) {
+			
+			gui.util.Alerts.showAlert("DepartmentList -> updateTableView","Service was null", null, AlertType.ERROR);
+		}else {
+			List<Department> list = service.findAll();
+			obsList = FXCollections.observableArrayList(list);
+			tableViewDepartment.setItems(obsList);
+		}
 	}
 
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -32,7 +33,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		 loadView("/gui/DepartmentList.fxml");
+		 loadView2("/gui/DepartmentList.fxml");
 	}
 
 	@FXML
@@ -69,4 +70,32 @@ public class MainViewController implements Initializable {
 		
 	}
 
+	
+private synchronized void loadView2(String absoluteName) {
+		
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVbox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVbox.getChildren().get(0);
+			
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVbox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setService(new DepartmentService());
+			controller.updateTableView();
+			
+		} catch (IOException e) {
+			
+			gui.util.Alerts.showAlert("IOException", e.getMessage(), "Erro ao carregar loadView()", AlertType.ERROR);
+		}
+		
+	}
+	
 }
