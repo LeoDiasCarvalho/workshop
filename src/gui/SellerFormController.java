dd257452557d81ca643187d19fc91b3f029048ac
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2022-03-05 16:46:44 -0300

    TextField e DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +19,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import modal.exceptions.ValidationException;
@@ -32,7 +35,13 @@ public class SellerFormController implements Initializable{
 	
 	@FXML private TextField txtId;
 	@FXML private TextField txtName;
+	@FXML private TextField txtEmail;
+	@FXML private DatePicker dpBirtDate;
+	@FXML private TextField txtBaseSalary;
 	@FXML private Label lblErrorName;
+	@FXML private Label lblErrorEmail;
+	@FXML private Label lblErrorBirthDate;
+	@FXML private Label lblErrorBaseSalary;
 	@FXML private Button btnSave;
 	@FXML private Button btnCancel;
 	
@@ -102,6 +111,13 @@ public class SellerFormController implements Initializable{
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if(entity.getBirthDate() != null) {
+			
+			dpBirtDate.setValue(LocalDate.ofInstant( entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+			
+		}
 	}
 	
 	
@@ -127,7 +143,10 @@ public class SellerFormController implements Initializable{
 	public void initializeNodes() {
 		
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldMaxLength(txtName, 50);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 50);
+		Utils.formatDatePicker(dpBirtDate, "dd/MM/yyyy");
 	}
 	
 	private void setErrorMenssages(Map<String, String> errors) {
